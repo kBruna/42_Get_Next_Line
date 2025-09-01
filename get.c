d31014b5ee8c4cb142e18a2545c8b5a3868ce2b5
buehara <buehara@student.42sp.org.br>
2025-09-01 18:08:38 -0300

    add get.c file~

--- get.c
+++ get.c
@@ -6,7 +6,7 @@
 /*   By: buehara <buehara@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/01 14:15:56 by buehara           #+#    #+#             */
-/*   Updated: 2025/09/01 14:50:10 by buehara          ###   ########.fr       */
+/*   Updated: 2025/09/01 17:44:20 by buehara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ t_link	*ft_new_node(t_link *prev)
 	node = malloc(sizeof(t_link));
 	if (!node)
 		return (NULL);
-	node->content = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	node->content = malloc(sizeof(char) * BUFFER_SIZE + 2);
 	if (!node->content)
 		return (NULL);
 	node->prev = prev;
@@ -27,31 +27,93 @@ t_link	*ft_new_node(t_link *prev)
 	return (node);
 }
 
-char	*get_next_line(int fd)
+int	ft_gnl_strlen(t_link *node, int *size)
+{
+	char	*cpy;
+	int		find;
+	int		index;
+
+	cpy = node->content;
+	find = 0;
+	index = 0;
+	while (cpy && cpy[index] != '\n' && cpy[index] != '\0')
+	{
+		(*size)++;
+		index++;
+		if (cpy[index] == '\n')
+			find = 1;
+	}
+	return (find);
+}
+
+char	*ft_gnl_strcpy(t_link *node, int size)
+{
+	char	*org;
+	char	*cpy;
+	int		ctrl;
+
+	org = node->content;
+	ctrl = 0;
+	cpy = malloc(sizeof(char) * size + 2);
+	while (org && ctrl < size && cpy[ctrl] != '\0')
+	{
+		cpy[ctrl] = org[ctrl];
+		ctrl++;
+	}
+	cpy[ctrl] = '\n';
+	ctrl++;
+	cpy[ctrl] = '\0';
+	return (cpy);
+}
+
+void	ft_free(t_link **buffer)
 {
 	t_link	*node;
+
+	node = *buffer;
+	*buffer = (*buffer)->prev;
+	while (node->prev != NULL)
+	{
+		free(node->content);
+		free(node);
+		if (!*buffer)
+			return ;
+		node = *buffer;
+		*buffer = (*buffer)->prev;
+	}
+}
+
+char	*get_next_line(int fd)
+{
+	static t_link	*node;
 	t_link	*prev;
 	int		size_read;
-	int		ctrl;
-	char	*p;
+	int		size;
+	int		find;
+	char	*line;
 
 	if (fd < 0)
 		return (NULL);
 	prev = NULL;
-	node = ft_new_node(prev);
-	size_read = read(fd, node->content, BUFFER_SIZE);
-	node->content[size_read] = '\0';
-	ctrl = 0;
-	p = malloc(256);
-	while (ctrl < size_read)
+	line = NULL;
+	size = 0;
+	while (!line)
 	{
-		p[ctrl] = node->content[ctrl];
-		ctrl++;
+		node = ft_new_node(prev);
+		size_read = read(fd, node->content, BUFFER_SIZE);
+		node->content[size_read] = '\0';
+		find = ft_gnl_strlen(node, &size);
+		if (find)
+			line = ft_gnl_strcpy(node, size);
+		if (prev)
+			prev->next = node;
+		else
+			prev = node;
 	}
-	p[ctrl] = '\0';
-	free(node->content);
-	free(node);
-	return (p);
+//	free(node->content);
+//	free(node);
+	ft_free(&node);
+	return (line);
 }
 
 #include <fcntl.h>
